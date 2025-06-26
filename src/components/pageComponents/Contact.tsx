7d16871cbf8106bf151b245e73c1971b1922b414
AndreSiboli <andresiboli@outlook.com>
2025-06-26 16:44:52 -0300

    style: adding animation to contact -v0.1.5

--- src/components/pageComponents/Contact.tsx
+++ src/components/pageComponents/Contact.tsx
@@ -1,15 +1,45 @@
+"use client";
+
+import { useLayoutEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
 import styles from "@/styles/pageComponents/root/Contact.module.scss";
 import FormContact from "../form/FormContact";
 import Img from "../utils/Img";
 
 import wallpaper from "@/assets/photos/nick-pliahas-pcMboyJs0Xo-unsplash.jpg";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Contact() {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const wallpaperRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      if (!parentRef.current || !wallpaperRef.current) return;
+
+      gsap.from(wallpaperRef.current, {
+        scrollTrigger: {
+          trigger: parentRef.current,
+          start: "top bottom",
+          end: '50% bottom',
+          scrub: 0.5,
+        },
+        scale: 0.8,
+      });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
+
   return (
-    <div className={styles.contact} id="contact">
+    <div className={styles.contact} id="contact" ref={parentRef}>
       <div className={styles.contact_container}>
-        <figure className={styles.contact_wallpaper}>
-          <Img src={wallpaper}  style={{objectPosition: 'center 35%'}}/>
+        <figure className={styles.contact_wallpaper} ref={wallpaperRef}>
+          <Img src={wallpaper} style={{ objectPosition: "center 35%" }} />
         </figure>
         <div className={styles.contact_form}>
           <FormContact />
